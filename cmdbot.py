f6239ed695cb9af418deb7f37aca7c8eee119721
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-05-03 19:56:35 +0300

    Update cmdbot.py

--- cmdbot.py
+++ cmdbot.py
@@ -13,7 +13,7 @@ cmdbot.remove_command('help')
 @cmdbot.command(pass_context=True)
 async def help(ctx): #main help command
     if ctx.message.author.id == "YOUR ID":
-        embed = discord.Embed(title="CMD-Bot", color=0xFF0000) # formatting help embed message
+        embed = discord.Embed(title="CMD-Bot", color=0xfff542) # formatting help embed message
         embed.add_field(name="```util.cmd <command>```", value="Execute CMD Command.", inline=False) # formatting help embed message 2
         embed.add_field(name="```util.help```", value="Will show help embed message.", inline=False) # formatting help embed message 3
         await cmdbot.send_message(ctx.message.channel, embed=embed) # sending embed message
