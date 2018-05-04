82a1785cac1f4e884644af0e7613dbd2314b71ae
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-05-04 15:51:10 +0300

    Update cmdbot.py

--- cmdbot.py
+++ cmdbot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 
 Client = discord.Client()
-cmdbot = commands.Bot(command_prefix="YOUR PREFIX", formatter=None)
+cmdbot = commands.Bot(command_prefix="util.", formatter=None)
 
 token = 'BOT TOKEN'
 
